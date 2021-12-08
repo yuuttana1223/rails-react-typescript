0ddb4adc9b5110753b4e20fa0c884ab4eb6d4756
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-09 00:12:55 +0900

    ✨ Add FoodsController with index

---
+++ api/app/controllers/api/v1/foods_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::FoodsController < ApplicationController
+  def index
+    restaurant = Restaurant.find(params[:restaurant_id])
+    foods = restaurant.foods
+    render json: foods, status: :ok
+  end
+end
