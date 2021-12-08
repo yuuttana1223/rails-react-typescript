81f4021f846371d9cdc1489504aea73c45c115d7
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-08 17:44:42 +0900

    ✨ Add method to model.

--- api/app/models/line_food.rb
+++ api/app/models/line_food.rb
@@ -4,4 +4,13 @@ class LineFood < ApplicationRecord
   belongs_to :order, optional: true
 
   validates :count, numericality: { greater_than: 0 }
+
+  # LineFood.activeと記述するとActiveRecord_Relationの形で返す
+  scope :active, -> { where(active: true) }
+  # LineFood.other_restaurant(2)
+  scope :other_restaurant, -> (picked_restaurant_id) { where.not(restaurant_id: picked_restaurant_id) }
+
+  def total_amount
+    food.price * count
+  end
 end
--- api/app/models/order.rb
+++ api/app/models/order.rb
@@ -2,4 +2,15 @@ class Order < ApplicationRecord
   has_many :line_foods
 
   validates :total_price, numericality: { greater_than: 0 }
+
+  def save_with_update_line_foods!(line_foods)
+    ActiveRecord::Base.transaction do
+      line_foods.each do |line_food|
+        # selfは.save_withの.の前のインスタンス
+        line_food.update_attributes!(active: false, order: self) 
+      end
+      self.save!
+    end
+  end
+
 end
