ed6ee1ec9e3f1263d975d6269eff4746e2e3bfbe
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-08 18:19:56 +0900

    🌱 Add seed for Restaurant an Food.

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |n|
+  restaurant = Restaurant.new(
+    name: "testレストラン_#{n + 1}",
+    fee: 100,
+    time_required: 10,
+  )
+
+  12.times do |m|
+    restaurant.foods.build(
+      name: "フード名_#{m + 1}",
+      price: 500,
+      description: "フード_#{m  +1}の説明文です。"
+    )
+  end
+
+  restaurant.save!
+end
