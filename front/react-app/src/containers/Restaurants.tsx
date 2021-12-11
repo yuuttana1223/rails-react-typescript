2b9a4c4704c21ab3e96fd88259ea6308b32af1ef
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-11 15:14:39 +0900

    ✨ 💄 Update Restaurants

--- front/react-app/src/containers/Restaurants.tsx
+++ front/react-app/src/containers/Restaurants.tsx
@@ -1,13 +1,115 @@
-import { useEffect, VFC } from "react";
-import { fetchRestaurants } from "utils/api";
+import { useEffect, useReducer, VFC } from "react";
+import styled from "styled-components";
+import { restaurantsReducer } from "reducers/restaurants";
+import { REQUEST_STATE } from "constants/request";
+import { restaurantsActionTypes } from "constants/restaurants";
+import { fetchRestaurants } from "apis/restaurants";
+
+import MainLogo from "images/logo.png";
+import MainCoverImage from "images/main-cover-image.png";
+import RestaurantImage from "images/restaurant-image.jpg";
+import { Skeleton } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const SHederWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const SMainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const SMainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const SMainCover = styled.img`
+  height: 600px;
+`;
+
+const SRestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const SRestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`;
+
+const SRestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const SMainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SSubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
+
+const initialState = {
+  fetchState: REQUEST_STATE.INITIAL,
+  restaurants: [],
+};
 
 export const Restaurants: VFC = () => {
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
   useEffect(() => {
-    fetchRestaurants().then((data) => console.log(data));
+    dispatch({
+      type: restaurantsActionTypes.FETCHING,
+    });
+    fetchRestaurants().then((data) => {
+      dispatch({
+        type: restaurantsActionTypes.FETCH_SUCCESS,
+        payload: {
+          restaurants: data ?? state.restaurants,
+        },
+      });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <>
-      <div>レストラン一覧</div>
+      <SHederWrapper>
+        <SMainLogoImage src={MainLogo} alt="main logo" />
+      </SHederWrapper>
+      <SMainCoverImageWrapper>
+        <SMainCover src={MainCoverImage} alt="main logo" />
+      </SMainCoverImageWrapper>
+
+      <SRestaurantsContentsList>
+        {state.fetchState === REQUEST_STATE.LOADING ? (
+          <>
+            <Skeleton variant="rectangular" width={450} height={300} />
+            <Skeleton variant="rectangular" width={450} height={300} />
+            <Skeleton variant="rectangular" width={450} height={300} />
+          </>
+        ) : (
+          state.restaurants.map((restaurant) => (
+            <Link
+              to={`/restaurants/${restaurant.id}/foods`}
+              key={restaurant.id}
+            >
+              <SRestaurantsContentWrapper>
+                <SRestaurantsImageNode src={RestaurantImage} />
+                <SMainText>{restaurant.name}</SMainText>
+                <SSubText>
+                  配送料: {restaurant.fee}円 {restaurant.time_required}分
+                </SSubText>
+              </SRestaurantsContentWrapper>
+            </Link>
+          ))
+        )}
+      </SRestaurantsContentsList>
     </>
   );
 };
