d0312ccc2a67eceacfa0d835e01112c67b7d0c91
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-14 01:00:59 +0900

    ⚡️ Update LineFoodsController index

--- api/app/controllers/api/v1/line_foods_controller.rb
+++ api/app/controllers/api/v1/line_foods_controller.rb
@@ -4,11 +4,21 @@ class Api::V1::LineFoodsController < ApplicationController
   def index
     line_foods = LineFood.active
     if line_foods.exists?
+      line_food_ids = []
+      count = 0
+      amount = 0
+    
+      line_foods.each do |line_food|
+        line_food_ids << line_food.id # (1) idを参照して配列に追加する
+        count += line_food[:count] # (2)countのデータを合算する
+        amount += line_food.total_amount # (3)total_amountを合算する
+      end
+    
       render json: {
-        line_food_ids: line_foods.map { |line_food| line_food.id },
+        line_food_ids: line_food_ids,
         restaurant: line_foods[0].restaurant,
-        count: line_foods.sum { |line_food| line_food[:count] },
-        amount: line_foods.sum { |line_food| line_food.total_amount },
+        count: count,
+        amount: amount,
       }, status: :ok
     else
       render json: {}, status: :no_content
